706f83ad198e9c432aa1fe88de22ce66c9d41bb8
ROSHAN1223 <roshanjadhav29121999@gmail.com>
2024-09-06 01:57:29 +0530

    WanderLust Backend
    WanderLust Backend

---
+++ routes/singlehotel.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const singlehotelHandler = require("../controllers/singleHoterController");
+
+router.route("/:id")
+    .get(singlehotelHandler)
+
+module.exports= router;
+
+
